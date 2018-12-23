33170406102f49a9c716eacb6b01d24e99b31890
Trukhonin Ilya <trukhonin-tin@dartit.ru>
2018-12-24 00:41:01 +0500

    disable drop-create option

--- src/main/java/coursework_helpdesk/config/DataConfig.java
+++ src/main/java/coursework_helpdesk/config/DataConfig.java
@@ -71,7 +71,7 @@ public class DataConfig {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        //properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
         properties.setProperty(
                 "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 
