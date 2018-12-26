f4171023c888e7ebd46b96b64d60390f7d73dc89
Trukhonin Ilya <trukhonin-tin@dartit.ru>
2018-12-27 01:02:33 +0500

    return all users

--- src/main/java/coursework_helpdesk/controllers/UserController.java
+++ src/main/java/coursework_helpdesk/controllers/UserController.java
@@ -4,9 +4,15 @@ import coursework_helpdesk.model.User;
 import coursework_helpdesk.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -21,7 +27,9 @@ public class UserController {
     }
 
     @GetMapping("")
-    public List<User> getAllUsers(){
-        return repository.findAll();
+    public Model getAllUsers(Model model) {
+        List<User> users = repository.findAll();
+        model.addAttribute("users",users);
+        return model;
     }
 }
